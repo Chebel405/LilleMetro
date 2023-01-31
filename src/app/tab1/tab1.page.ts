0417873a66fa0b6e9c006d268d2956cfdc46d4ef
Chebel405 <anne.chebel@gmail.com>
2023-01-31 11:41:08 +0100

    Ajout de l'Api

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
 
-  constructor() {}
+export class Tab1Page {
+  ApiKey = '5835265ca4b30f8639aebe957d08b93ff78b34e1453cfee0b56d8cef';
+  infoData = {
+    
+  }
+
+
+  constructor(public http: HttpClient) {
+
+  }
+
+  readApi(URL: string) {
+    return this.http.get(URL);
+  }
 
 }
